486011bc883c51c6b5f8bf8ca47bd957d0fb4dea
Fernando Vega <pytness@gmail.com>
2022-10-05 15:55:09 +0200

    Remove comments

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -117,103 +117,24 @@ lvim.builtin.treesitter.ensure_installed = {
     "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
+vim.g.palenight_color_overrides = {
+    --     -- black = { gui = '#000000', cterm = "0", cterm16 = "0" },
+    --     blue = { gui = "#8cb8ff", cterm = "39", cterm16 = "4" },
+    --     purple = { gui = "#d67cde", cterm = "170", cterm16 = "5" },
+    --     blue_purple = { gui = "#8a87de", cterm = "39", cterm16 = "4" },
+    --     cyan = { gui = "#96e1ff", cterm = "38", cterm16 = "6" },
+    --     white = { gui = "#e6efff", cterm = "145", cterm16 = "7" },
+    cursor_grey = { gui = "#3E4452", cterm = 236, cterm16 = "8" },
 
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumeko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
-
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
-
--- vim.g.palenight_color_overrides = {
---     -- black = { gui = '#000000', cterm = "0", cterm16 = "0" },
---     blue = { gui = "#8cb8ff", cterm = "39", cterm16 = "4" },
---     purple = { gui = "#d67cde", cterm = "170", cterm16 = "5" },
---     blue_purple = { gui = "#8a87de", cterm = "39", cterm16 = "4" },
---     cyan = { gui = "#96e1ff", cterm = "38", cterm16 = "6" },
---     white = { gui = "#e6efff", cterm = "145", cterm16 = "7" },
--- }
+}
 -- vim.g.palenight_termcolors = 16;
 vim.g.palenight_terminal_italics = 1;
 
 -- Additional Plugins
 lvim.plugins = {
     { "lunarvim/colorschemes" },
-    --    {"folke/tokyonight.nvim"},
-    --    {"folke/trouble.nvim", cmd = "TroubleToggle",},
-    --    {"nvim-treesitter/nvim-treesitter"},
-    --     {"nvim-treesitter/nvim-treesitter-context"},
-    --     {"airblade/vim-gitgutter"},
     { "pytness/palenight.vim" },
     { "github/copilot.vim" },
     {
@@ -225,21 +146,16 @@ lvim.plugins = {
             }
         end
     },
-    --    {"nvim-lualine/lualine.nvim"},
-    --     {"kyazdani42/nvim-web-devicons"},
-    --     {"kyazdani42/nvim-tree.lua"},
     { "lukas-reineke/indent-blankline.nvim" },
-
-    -- { "edluffy/hologram.nvim" },
     { "gen740/SmoothCursor.nvim",
         event = "WinScrolled",
         config = function()
             require('smoothcursor').setup {
                 autostart = true,
                 cursor = "", -- cursor shape (need nerd font)
-                intervals = 35, -- tick interval
+                intervals = 15, -- tick interval
                 linehl = nil, -- highlight sub-cursor line like 'cursorline', "CursorLine" recommended
-                type = "default", -- define cursor movement calculate function, "default" or "exp" (exponential).
+                type = "exp", -- define cursor movement calculate function, "default" or "exp" (exponential).
                 fancy = {
                     enable = true, -- enable fancy mode
                     head = { cursor = "", texthl = "SmoothCursor", linehl = nil },
