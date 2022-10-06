be25c613d8f8de5321d9f0569185ab3da260c9a3
Fernando Vega <pytness@gmail.com>
2022-10-06 11:07:12 +0200

    Remove comments

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,19 +2,7 @@ local home_dir = os.getenv("HOME")
 package.path = home_dir .. "/.config/lvim/?.lua;" .. package.path
 
 require "custom.dashboard"
--- dashboard.opts.opts.noautocmd = true
 
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
--- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "palenight"
@@ -52,42 +40,6 @@ vim.cmd("set cmdheight=1")
 
 vim.opt.cursorline = true
 
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
--- unmap a default keymapping
--- vim.keymap.del("n", "<C-Up>")
--- override a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -214,13 +166,9 @@ cmp.setup {
     }
 }
 
--- vim.diagnostic.config({ signs = false })
--- vim.lsp.diagnostic.disable()
-
 vim.g.lightspeed_no_default_keymaps = 1
 vim.cmd("nmap s <Plug>Lightspeed_omni_s")
 
---
 require("telescope").setup({
     defaults = {
         preview = {
@@ -252,48 +200,3 @@ require("telescope").setup({
         },
     }
 })
-
-
--- require('lualine').setup {
---     options = {
---         theme = 'papercolor_light',
---         component_separators = '|',
---         section_separators = { left = '', right = '' },
---     },
---     sections = {
---         lualine_a = {
---             { 'mode', separator = { left = '' }, right_padding = 2 },
---         },
---         lualine_b = { 'filename', 'branch' },
---         lualine_c = { 'fileformat' },
---         lualine_x = {},
---         lualine_y = { 'filetype', 'progress' },
---         lualine_z = {
---             { 'location', separator = { right = '' }, left_padding = 2 },
---         },
---     },
---     inactive_sections = {
---         lualine_a = { 'filename' },
---         lualine_b = {},
---         lualine_c = {},
---         lualine_x = {},
---         lualine_y = {},
---         lualine_z = { 'location' },
---     },
---     tabline = {},
---     extensions = {},
--- }
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
