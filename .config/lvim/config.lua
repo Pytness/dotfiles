02addc76349db47201a6b453eeb40718f4a1c312
Fernando Vega <pytness@gmail.com>
2022-10-17 16:17:35 +0200

    Fix multiple encodings error

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -67,6 +67,12 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.highlight.enabled = true
 
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+local capabilities = require("lvim.lsp").common_capabilities()
+capabilities.offsetEncoding = { "utf-8" }
+local opts = { capabilities = capabilities }
+require("lvim.lsp.manager").setup("clangd", opts)
+
 vim.g.palenight_color_overrides = {
     --     -- black = { gui = '#000000', cterm = "0", cterm16 = "0" },
     --     blue = { gui = "#8cb8ff", cterm = "39", cterm16 = "4" },
