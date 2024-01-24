54287b7c40eb5d88003937478d31520c8c6b16c2
Fernando Vega <fernando.vega@fcc.es>
2024-01-24 10:27:44 +0100

    Change vi mode colors

--- .p10k.zsh
+++ .p10k.zsh
@@ -808,13 +808,15 @@
   typeset -g POWERLEVEL9K_VI_MODE_NORMAL_BACKGROUND=2
   # Text and color for visual vi mode.
   typeset -g POWERLEVEL9K_VI_VISUAL_MODE_STRING=VISUAL
+  typeset -g POWERLEVEL9K_VI_MODE_VISUAL_FOREGROUND=15
   typeset -g POWERLEVEL9K_VI_MODE_VISUAL_BACKGROUND=4
   # Text and color for overtype (a.k.a. overwrite and replace) vi mode.
   typeset -g POWERLEVEL9K_VI_OVERWRITE_MODE_STRING=OVERTYPE
   typeset -g POWERLEVEL9K_VI_MODE_OVERWRITE_BACKGROUND=3
   # Text and color for insert vi mode.
-  typeset -g POWERLEVEL9K_VI_INSERT_MODE_STRING=
-  typeset -g POWERLEVEL9K_VI_MODE_INSERT_FOREGROUND=8
+  typeset -g POWERLEVEL9K_VI_INSERT_MODE_STRING=INSERT
+  typeset -g POWERLEVEL9K_VI_MODE_INSERT_FOREGROUND=15
+  typeset -g POWERLEVEL9K_VI_MODE_INSERT_BACKGROUND=12
 
   ######################################[ ram: free RAM ]#######################################
   # RAM color.
