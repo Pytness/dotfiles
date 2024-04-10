52942a650a840e3fb47ce60d1ba0fabb3e881d87
Fernando Vega <pytness@gmail.es>
2024-04-10 16:56:27 +0200

    Add Lazy related keybinds

--- .config/nvim/custom/keybinds/neovim.lua
+++ .config/nvim/custom/keybinds/neovim.lua
@@ -1,2 +1,4 @@
 -- Neovim keymaps
 vim.keymap.set('n', '<leader>nc', '<cmd>e $MYVIMRC<cr>', { desc = 'Open init.lua' })
+vim.keymap.set('n', '<leader>ns', '<cmd>Lazy sync<cr>', { desc = 'Lazy sync' })
+vim.keymap.set('n', '<leader>nu', '<cmd>Lazy update<cr>', { desc = 'Lazy update' })
