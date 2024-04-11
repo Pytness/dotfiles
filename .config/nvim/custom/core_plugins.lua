1d883df8daedf2458024131f2243352ca1b03d45
Fernando Vega <pytness@gmail.es>
2024-04-11 13:49:14 +0200

    Remove luarocks and neorg

--- .config/nvim/custom/core_plugins.lua
+++ .config/nvim/custom/core_plugins.lua
@@ -565,38 +565,4 @@ return {
   {
     'ThePrimeagen/vim-be-good',
   },
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000,
-    config = true,
-  },
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000, -- We'd like this plugin to load first out of the rest
-    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-  },
-  {
-    'nvim-neorg/neorg',
-    dependencies = { 'luarocks.nvim' },
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = '*', -- Pin Neorg to the latest stable release
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {}, -- Loads default begaviour
-          ['core.concealer'] = {},
-          ['core.dirman'] = {
-            config = {
-              workspaces = {
-                notes = '~/notes',
-              },
-              default_workspace = 'notes',
-            },
-          },
-        },
-      }
-    end,
-    run = ':Neorg sync-parsers',
-    requires = 'nvim-lua/plenary.nvim',
-  },
 }
