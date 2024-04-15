d234f30cf244305cbc7a4c31365cc581896e8bd9
Fernando Vega <pytness@gmail.es>
2024-04-15 16:57:47 +0200

    Minor keybind changes

--- .config/nvim/custom/keybinds/neovim.lua
+++ .config/nvim/custom/keybinds/neovim.lua
@@ -1,4 +1,7 @@
+local config_path = vim.fn.stdpath 'config'
 -- Neovim keymaps
 vim.keymap.set('n', '<leader>nc', '<cmd>e $MYVIMRC<cr>', { desc = 'Open init.lua' })
+vim.keymap.set('n', '<leader>nC', '<cmd>cd ' .. config_path .. '<cr>', { desc = 'Open init.lua' })
+
 vim.keymap.set('n', '<leader>ns', '<cmd>Lazy sync<cr>', { desc = 'Lazy sync' })
 vim.keymap.set('n', '<leader>nu', '<cmd>Lazy update<cr>', { desc = 'Lazy update' })
