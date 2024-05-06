a5ea876c0611805c3b7027f2473affbf6ba6da2f
Fernando Vega <pytness@gmail.es>
2024-05-06 10:29:34 +0200

    Move venn keymap to misc

--- .config/nvim/custom/keybinds.lua
+++ .config/nvim/custom/keybinds.lua
@@ -50,25 +50,3 @@ require 'custom.keybinds.lsp'
 require 'custom.keybinds.search'
 
 require 'custom.keybinds.zen'
-
--- venn.nvim: enable or disable keymappings
-local venn_enabled = false
-local function Toggle_venn()
-  if venn_enabled == false then
-    venn_enabled = true
-    vim.cmd [[setlocal ve=all]]
-    -- draw a line on HJKL keystokes
-    vim.api.nvim_buf_set_keymap(0, 'n', 'J', '<C-v>j:VBox<CR>', { noremap = true })
-    vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<C-v>k:VBox<CR>', { noremap = true })
-    vim.api.nvim_buf_set_keymap(0, 'n', 'L', '<C-v>l:VBox<CR>', { noremap = true })
-    vim.api.nvim_buf_set_keymap(0, 'n', 'H', '<C-v>h:VBox<CR>', { noremap = true })
-    -- draw a box by pressing "f" with visual selection
-    vim.api.nvim_buf_set_keymap(0, 'v', 'f', ':VBox<CR>', { noremap = true })
-  else
-    vim.cmd [[setlocal ve=]]
-    vim.cmd [[mapclear <buffer>]]
-    venn_enabled = false
-  end
-end
--- toggle keymappings for venn using <leader>v
-vim.keymap.set('n', '<leader>v', Toggle_venn, { desc = 'Toggle venn', noremap = true })
--- .config/nvim/custom/keybinds/misc.lua
+++ .config/nvim/custom/keybinds/misc.lua
@@ -1,1 +1,23 @@
+-- venn.nvim: enable or disable keymappings
+local venn_enabled = false
+local function Toggle_venn()
+  if venn_enabled == false then
+    venn_enabled = true
+    vim.cmd [[setlocal ve=all]]
+    -- draw a line on HJKL keystokes
+    vim.api.nvim_buf_set_keymap(0, 'n', 'J', '<C-v>j:VBox<CR>', { noremap = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<C-v>k:VBox<CR>', { noremap = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'L', '<C-v>l:VBox<CR>', { noremap = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'H', '<C-v>h:VBox<CR>', { noremap = true })
+    -- draw a box by pressing "f" with visual selection
+    vim.api.nvim_buf_set_keymap(0, 'v', 'f', ':VBox<CR>', { noremap = true })
+  else
+    vim.cmd [[setlocal ve=]]
+    vim.cmd [[mapclear <buffer>]]
+    venn_enabled = false
+  end
+end
+
+vim.keymap.set('n', '<leader>mv', Toggle_venn, { desc = 'Toggle venn', noremap = true })
+
 vim.keymap.set('n', '<leader>mm', '<cmd>MarkdownPreviewToggle<cr>', { desc = 'Toggle markdown preview' })
