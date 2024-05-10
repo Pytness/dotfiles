0a456004dcba7e2340bd7d28658a2f32e5da4bd3
Fernando Vega <pytness@gmail.es>
2024-05-10 11:05:14 +0200

    Add harpoon keymaps

---
+++ .config/nvim/custom/keybinds/harpoon.lua
@@ -0,0 +1,47 @@
+local harpoon = require 'harpoon'
+-- local harpoon_ext = require 'harpoon.ext'
+
+local function add()
+  harpoon:list():add()
+end
+
+vim.keymap.set('n', '<leader>ha', add, { desc = 'Add current file to Harpoon' })
+
+local function toggle_telescope(harpoon_files)
+  -- local opts = {
+  --   attach_mappings = function(_, map)
+  --     map('i', '<A-j>', 'move_selection_next')
+  --     return true
+  --   end,
+  -- }
+  -- require('telescope').extensions.harpoon.marks()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end
+
+vim.keymap.set('n', '<leader>he', function()
+  toggle_telescope(harpoon:list())
+end, { desc = 'Toggle Harpoon menu' })
+
+vim.keymap.set('n', '<leader>hh', function()
+  harpoon:list():select(1)
+end, { desc = 'Select Harpoon 1' })
+
+vim.keymap.set('n', '<leader>hj', function()
+  harpoon:list():select(2)
+end, { desc = 'Select Harpoon 2' })
+
+vim.keymap.set('n', '<leader>hk', function()
+  harpoon:list():select(3)
+end, { desc = 'Select Harpoon 3' })
+
+vim.keymap.set('n', '<leader>hl', function()
+  harpoon:list():select(4)
+end, { desc = 'Select Harpoon 4' })
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set('n', '<leader>hp', function()
+  harpoon:list():prev()
+end, { desc = 'Select previous Harpoon buffer' })
+vim.keymap.set('n', '<leader>hn', function()
+  harpoon:list():next()
+end, { desc = 'Select next Harpoon buffer' })
