ced4b53cdea7e295b2ffedc5c305f658b4ac1fb7
Fernando Vega <pytness@gmail.es>
2024-05-27 11:07:30 +0200

    Add debug statement to git restore file

--- .config/nvim/custom/keybinds/git.lua
+++ .config/nvim/custom/keybinds/git.lua
@@ -3,7 +3,9 @@ local gittools = require 'custom.gittools'
 -- git restore current file and reload it
 local function restore_current_file()
   local current_file = vim.api.nvim_buf_get_name(0)
+
   gittools.restore_file(current_file)
+  print('Restored ' .. current_file)
 
   vim.cmd 'edit!'
 end
