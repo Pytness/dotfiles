94c8b18739132839a31bcd19430013c98efed805
Fernando Vega <pytness@gmail.es>
2024-05-27 10:01:52 +0200

    Increase timeoutlen

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -53,7 +53,7 @@ vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 100
+vim.opt.timeoutlen = 250
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
