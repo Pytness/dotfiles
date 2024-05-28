49881bc43902b2707dffea83f1262ca246e18662
Fernando Vega <pytness@gmail.es>
2024-05-28 17:39:12 +0200

    Add keymap for clearing diagnostics

--- .config/nvim/custom/keybinds/lsp.lua
+++ .config/nvim/custom/keybinds/lsp.lua
@@ -2,6 +2,7 @@ local telescope_builtin = require 'telescope.builtin'
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>lk', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', '<leader>lK', vim.diagnostic.reset, { desc = 'Clear all diagnostics' })
 vim.keymap.set('n', '<leader>lj', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 
 vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
