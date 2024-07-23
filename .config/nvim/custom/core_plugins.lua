d394d9db36e6c77f70630c1a1c27b7779df90f77
Fernando Vega <pytness@gmail.com>
2024-07-23 15:05:11 +0200

    Add hardtime plugin

--- .config/nvim/custom/core_plugins.lua
+++ .config/nvim/custom/core_plugins.lua
@@ -680,4 +680,16 @@ return {
       require('alpha').setup(dash.opts)
     end,
   },
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    opts = {
+      disabled_keys = {
+        ['<Esc>'] = { 'i' },
+        -- Enable scrolling
+        ['<Up>'] = { 'i' },
+        ['<Down>'] = { 'i' },
+      },
+    },
+  },
 }
