2f9c04cfe0bd2516ea5ffcf33a464edf486d9994
Fernando Vega <pytness@gmail.com>
2024-07-23 17:57:20 +0200

    Merge hyprdots upstream

--- .config/hypr/scripts/dontkillsteam.sh
+++ .config/hypr/scripts/dontkillsteam.sh
@@ -1,12 +1,4 @@
-function contains() {
-    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
-}
-
-window_class=$(hyprctl activewindow -j | jq -r ".class")
-
-dont_close_classes="Steam Enpass"
-
-if [ $(contains $dont_close_classes $window_class) -ne 0 ]; then
+if [[ $(hyprctl activewindow -j | jq -r ".class") == "Steam" ]]; then
     xdotool windowunmap $(xdotool getactivewindow)
 else
     hyprctl dispatch killactive ""
