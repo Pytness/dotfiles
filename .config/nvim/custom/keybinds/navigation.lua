79d84c33be92441457eff9e6c5ca5038dda53c7d
Fernando Vega <pytness@gmail.com>
2024-09-03 11:11:58 +0200

    Use bufferline keymaps

--- .config/nvim/custom/keybinds/navigation.lua
+++ .config/nvim/custom/keybinds/navigation.lua
@@ -5,7 +5,14 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Buffer navigation
-vim.keymap.set('n', '<leader>bb', '<cmd>bprev<cr>', { desc = 'Previous buffer' })
-vim.keymap.set('n', '<leader>bn', '<cmd>bnext<cr>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bb', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>bn', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer' })
+
+vim.keymap.set('n', '<leader>bk', '<cmd>BufferLineMovePrev<cr>', { desc = 'Move prev' })
+vim.keymap.set('n', '<leader>bj', '<cmd>BufferLineMoveNext<cr>', { desc = 'Move next' })
+
+vim.keymap.set('n', '<leader>bH', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all to the left' })
+vim.keymap.set('n', '<leader>bL', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all to the right' })
+
 -- vim.keymap.set('n', '<leader>c', '<cmd>BufDel<cr>', { desc = 'Close Buffer' })
 vim.keymap.set('n', '<leader>c', '<cmd>bd!<cr>', { desc = 'Close Buffer' })
