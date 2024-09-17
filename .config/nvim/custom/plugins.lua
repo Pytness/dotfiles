bdf71cefaecdd918cfd88a84cf36f13d3ae91545
Fernando Vega <pytness@gmail.com>
2024-09-17 10:42:41 +0200

    Limit treesitter context size

--- .config/nvim/custom/plugins.lua
+++ .config/nvim/custom/plugins.lua
@@ -189,6 +189,14 @@ return {
     opts = {
       after = 'nvim-treesitter',
     },
+    config = function()
+      require('treesitter-context').setup {
+        max_lines = 3,
+        trim_scope = 'inner',
+        multiline_threshold = 1,
+        mode = 'topline',
+      }
+    end,
   },
   {
     'ThePrimeagen/harpoon',
