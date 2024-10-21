1bded079ac650c2255a4d4cd01631229a2c1f6cc
Fernando Vega <pytness@gmail.com>
2024-10-21 10:06:11 +0200

    Move lazy keymaps to separate key

--- .config/nvim/custom/keymaps/neovim.lua
+++ .config/nvim/custom/keymaps/neovim.lua
@@ -3,8 +3,10 @@ local config_path = vim.fn.stdpath 'config'
 return {
   { 'n', '<leader>nc', '<cmd>e $MYVIMRC<cr>', { desc = 'Open init.lua' } },
   { 'n', '<leader>nC', '<cmd>cd ' .. config_path .. '<cr>', { desc = 'Open init.lua' } },
-  { 'n', '<leader>ns', '<cmd>Lazy sync<cr>', { desc = 'Lazy sync' } },
-  { 'n', '<leader>nl', '<cmd>Lazy<cr>', { desc = 'Lazy' } },
-  { 'n', '<leader>nu', '<cmd>Lazy update<cr>', { desc = 'Lazy update' } },
   { 'n', '<leader>nr', '<cmd>source $MYVIMRC<cr>', { desc = 'Re source init.lua' } },
+
+  { 'n', '<leader>Ls', '<cmd>Lazy sync<cr>', { desc = 'Lazy sync' } },
+  { 'n', '<leader>Ll', '<cmd>Lazy<cr>', { desc = 'Lazy' } },
+  { 'n', '<leader>Lu', '<cmd>Lazy update<cr>', { desc = 'Lazy update' } },
+  { 'n', '<leader>Lp', '<cmd>Lazy profile<cr>', { desc = 'Lazy profile' } },
 }
