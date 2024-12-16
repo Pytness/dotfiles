7d3afe95147eef350eaef3a415c25702188ce6ad
Fernando Vega <pytness@gmail.com>
2024-12-16 21:46:58 +0100

    Fix incorrect method for multiline buffer deletion

--- .config/nvim/keymaps/bafa.lua
+++ .config/nvim/keymaps/bafa.lua
@@ -10,6 +10,10 @@ local function delete_menu_item()
   require('bafa.ui').delete_menu_item()
 end
 
+local function delete_multiple_menu_items()
+  require('bafa.ui').delete_multiple_menu_items()
+end
+
 return {
   { 'n', 'q', toggle, { silent = true } },
   { 'n', '<ESC>', toggle, { silent = true } },
@@ -17,5 +21,5 @@ return {
   { 'n', '<C-l>', select_menu_item, {} },
   { 'n', 'dd', delete_menu_item, {} },
   { 'n', 'D', delete_menu_item, {} },
-  { 'v', 'd', delete_menu_item, {} },
+  { 'v', 'd', delete_multiple_menu_items, {} },
 }
