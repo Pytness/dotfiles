32414673d0dadc3af981eeab60bd462625883980
Fernando Vega <pytness@gmail.com>
2025-02-17 16:34:55 +0100

    Remove lvim config

--- .config/lvim/custom/lsp.lua
+++
@@ -1,5 +0,0 @@
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
-
-lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-	return server ~= "pylsp"
-end, lvim.lsp.automatic_configuration.skipped_servers)
